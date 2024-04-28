80c758abf9a14d2e68239bee123ab782690b262c
shahriararefin <shahriararefin7@gmail.com>
2024-04-28 10:59:57 +0600

    push and pop in js

---
+++ Projects/Module 17/push&pop.js
@@ -0,0 +1,15 @@
+var numbers = [15, 65, 9, 10];
+
+numbers.push(62); 
+
+console.log(numbers);
+
+var friends= ["ased", "awfs"];
+
+friends.push("bavae");
+console.log(friends);
+numbers.pop();
+console.log(numbers);
+
+friends.pop();
+console.log(friends);
