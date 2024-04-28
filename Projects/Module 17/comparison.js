e3f759910bf10e42b4d865d8295c571ecef1f931
shahriararefin <shahriararefin7@gmail.com>
2024-04-28 11:53:46 +0600

    comparisons

---
+++ Projects/Module 17/comparison.js
@@ -0,0 +1,18 @@
+// console.log(5 < 6);
+// console.log(5 > 6);
+
+
+var money = 10;
+var danishPrice = 45;
+var biscuits = 25;
+
+if (money > danishPrice) {
+    console.log("Mama Danish den");
+}
+
+else if(money > biscuits) {
+    console.log("Mama Biscuits den");
+}
+else {
+    console.log("Mama taka to kom pore bakite dile den naile jaiga");
+}
