e0e08ab61427b4f6cee4f3dd0cc2321d6615b9a4
shahriararefin <shahriararefin7@gmail.com>
2024-04-29 12:55:01 +0600

    condition and array

---
+++ Projects/Module 18/array&conditional.js
@@ -0,0 +1,29 @@
+var bottle = 'water';
+
+var bottlecolor = 'yellow';
+var sunglassPrice = 350;
+
+var penColor = 'Blue';
+
+var isExpensive = true;
+
+var onBed = ['Book', 'Calculator', 'Phone', 'Charger', 'Airpod', 'Mouse', 'Remote'];
+
+console.log(onBed[2]);
+
+var mouseIndex = onBed.indexOf('Mouse');
+
+console.log(mouseIndex);
+
+if (onBed[2]=='Book'){
+    console.log('Yes');
+}
+else if (onBed[2]=='Calculator'){
+    console.log('Yes, Got ' + onBed[2]);
+}
+else {
+    console.log('No');
+}
+
+
+
