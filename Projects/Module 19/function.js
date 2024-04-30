a48f59b7a4cd96ecd2c2bd0a1aecb2679585b0d0
shahriararefin <shahriararefin7@gmail.com>
2024-04-30 12:56:53 +0600

    function in javascript

---
+++ Projects/Module 19/function.js
@@ -0,0 +1,11 @@
+function turnOnLight(){
+    console.log("Stand up");
+    
+    console.log("Get to the switch");
+
+    console.log("Press the first switch from right");
+
+    console.log("The light is on");
+}
+
+turnOnLight();
