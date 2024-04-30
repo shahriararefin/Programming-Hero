79ba04208a0afdc289ef1d2bc2b46fc582751261
shahriararefin <shahriararefin7@gmail.com>
2024-04-30 13:14:29 +0600

    function with parameter

--- Projects/Module 19/funcwithparameter.js
+++ Projects/Module 19/funcwithparameter.js
@@ -10,3 +10,16 @@ function buyJuice(money) {
 var money = 40;
 var mangoJuice = 50;
 buyJuice(money);
+
+
+function addMoney(table, moneybag){
+
+    console.log("Add money from table and moneybag= " +money, "From Table: " + table, "From Monehybag: " + moneybag);
+}
+
+var table = 50;
+
+var moneybag = 120;
+var money = table + moneybag;
+
+addMoney(table, moneybag);
