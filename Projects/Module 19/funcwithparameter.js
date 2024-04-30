d2974a5309d4b5d36768b583e7bdd0b2c7d30001
shahriararefin <shahriararefin7@gmail.com>
2024-04-30 13:05:53 +0600

    function with parameters

---
+++ Projects/Module 19/funcwithparameter.js
@@ -0,0 +1,12 @@
+function buyJuice(money) {
+    if (money >= mangoJuice){
+        console.log("Bought Mango Juice");
+        }else{
+        console.log("Not enough money");
+    }
+    
+}
+
+var money = 40;
+var mangoJuice = 50;
+buyJuice(money);
