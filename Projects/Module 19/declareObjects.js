50ad6d29ab43d2a2f0d90841c424622a6c8805b1
shahriararefin <shahriararefin7@gmail.com>
2024-05-01 17:47:38 +0600

    property values and property name

---
+++ Projects/Module 19/declareObjects.js
@@ -0,0 +1,20 @@
+
+// declare object name student and its properties
+var student ={
+    name: "Student",
+    id: 321,
+    batch: 32,
+    address: "Dhaka"
+}
+
+
+// console.log(student);
+// console.log(student.name);
+// console.log(student.id);
+
+var properties = Object.keys(student);
+
+var values = Object.values(student);
+
+console.log(properties);
+console.log(values);
