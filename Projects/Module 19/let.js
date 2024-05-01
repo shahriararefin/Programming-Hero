1d09c639914bcee95e93f97460449d0a9499b596
shahriararefin <shahriararefin7@gmail.com>
2024-05-01 17:05:25 +0600

    declaring let

---
+++ Projects/Module 19/let.js
@@ -0,0 +1,6 @@
+let x,y;
+x= 5+6;
+y= x*10;
+
+console.log(x);
+console.log(y);
---
+++ Projects/Module 19/toCelcius.js
@@ -0,0 +1,6 @@
+function toCelcius(fahrenheit) {
+    return (fahrenheit - 32) * (5 / 9);
+} 
+
+let value = toCelcius(77);
+console.log(value);
