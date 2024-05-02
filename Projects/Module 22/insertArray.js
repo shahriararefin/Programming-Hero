123e16a052e24d200f5dbc4b3ac987048f0ee80b
shahriararefin <shahriararefin7@gmail.com>
2024-05-02 19:11:30 +0600

    partial

---
+++ Projects/Module 22/insertArray.js
@@ -0,0 +1,11 @@
+// get part of an array and insert elements using slice and splice
+
+
+const friends = [ 123, 13, 123, 51 ,5,3,51,61,12,63];
+
+const partial = friends.slice(1,5);
+
+console.log(partial);
+
+
+ 
---
+++ Projects/Module 22/isArray.js
@@ -0,0 +1,12 @@
+const country = "Something";
+
+const  age= 53;
+
+const isIndependent = true;
+
+console.log(isIndependent);
+console.log(typeof isIndependent);
+
+const person = [12, 123, 41 , 42];
+
+console.log(Array.isArray(person));
