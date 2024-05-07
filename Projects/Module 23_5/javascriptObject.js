0062480b86982bbdd394c9f6631c93537f90e113
shahriararefin <shahriararefin7@gmail.com>
2024-05-07 06:35:13 +0600

    things we can do with objects

---
+++ Projects/Module 23_5/javascriptObject.js
@@ -0,0 +1,51 @@
+const student={
+    name: 'Shakib Khan',
+    id: 132,
+    address: 'Panchagarh',
+    isSingle: true,
+    act: function actor(){
+        console.log('Action: blah blah blah');
+    }
+
+    friends: ['n', ' a', 'b', 'q'];
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
