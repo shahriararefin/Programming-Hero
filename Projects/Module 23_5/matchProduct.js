17ddbf875308a0f9206ddb08ce95eabf1a1955a4
shahriararefin <shahriararefin7@gmail.com>
2024-05-07 06:56:22 +0600

    search by matching products

--- Projects/Module 23_5/matchProduct.js
+++ Projects/Module 23_5/matchProduct.js
@@ -12,7 +12,7 @@ const numbers = [ 45, 65, 43, 72, 23];
 
 const products = [
     {id: 1, name: 'Xiaomi', price: 23000},
-    {id: 2, name: 'Apple macbook', price: 123000},
+    {id: 2, name: 'Apple Macbook', price: 123000},
     {id: 3, name: 'Samsung', price: 125000},
     {id: 4, name: 'Realme', price: 53000},
     {id: 5, name: 'Nokia', price: 43000},
@@ -27,11 +27,15 @@ const products = [
 // }
 
 function matchedProducts(products, search){
-    
+    const matched = [];
     for(const product of products){
-        console.log(product);
+        if(product.name.toLowerCase().includes(search.toLowerCase())){
+
+            matched.push(product);
+        }
     }
+    return matched;
 }
 
-const result = matchedProducts(products, 'macbook');
+const result = matchedProducts(products, 'nokia');
 console.log(result);
