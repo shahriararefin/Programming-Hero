094257ca7bce4d7295c94adecbdcb0507d477752
Shahriar Arefin <65588285+shahriararefin@users.noreply.github.com>
2024-08-03 17:17:05 +0600

    Add files via upload

--- Projects/Module 25/jsDomEvents/home.js
+++ Projects/Module 25/jsDomEvents/home.js
@@ -1,21 +1,61 @@
+// Deposit
+
 document.getElementById('submitDeposit').addEventListener('click', function(){
     const submitDepositAmount = document.getElementById('deposit');
 
     const deposited = submitDepositAmount.value;
 
-    console.log(deposited);
+    const totalDeposited = document.getElementById('total-deposited'); 
 
-    const totalDeposited = document.getElementById('total-deposited');
-    totalDeposited.innerText= deposited;
+    const previousDeposit = totalDeposited.innerText;
+    const newCurrentDeposit = parseFloat(previousDeposit) +  parseFloat(deposited);
+    
+    totalDeposited.innerText = newCurrentDeposit;
     //clear the deposit input field
 
     submitDepositAmount.value = '';
-})
 
+
+    // Update Balance
+
+    const balance= document.getElementById('balance');
+    const balanceText = balance.innerText;
+
+    const previousBalance = parseFloat(balanceText);
+    const newBalance= previousBalance + parseFloat(deposited);
+    
+    balance.innerText = newBalance;
+
+    
+});
+
+// Withdraw
 document.getElementById('submitWithdraw').addEventListener('click', function(){
     const submitWithdrawAmount = document.getElementById('withdraw');
+    
 
     const withdrawed = submitWithdrawAmount.value;
+    
+    const totalWithdrawal = document.getElementById('total-withdrawed'); 
 
-    console.log(withdrawed);
+    const previousWithdrawals = totalWithdrawal.innerText;
+    
+    const newCurrentWithdraw = parseFloat(previousWithdrawals)+  parseFloat(withdrawed);
+
+    totalWithdrawal.innerText = newCurrentWithdraw;
+    //clear the withdraw input field
+
+    submitWithdrawAmount.value = '';
+
+    // Update Balance
+
+    const balance = document.getElementById('balance');
+    const balanceText = balance.innerText;
+
+    const previousBalance = parseFloat(balanceText);
+    const newBalance = previousBalance - withdrawed;
+    
+    balance.innerText = newBalance;
+
 })
+
