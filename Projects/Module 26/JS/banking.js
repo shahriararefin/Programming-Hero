918c07ded47810cf7ed2e9403a9409d671f7356c
shahriararefin <shahriararefin7@gmail.com>
2024-08-04 19:30:39 +0600

    errors handled

--- Projects/Module 26/JS/banking.js
+++ Projects/Module 26/JS/banking.js
@@ -1,5 +1,5 @@
 function getSubmitValue(submitId){
-
+    debugger;
     const submitAmount = document.getElementById(submitId);
     const submittedText = submitAmount.value;
     const submitted = parseFloat(submittedText);
@@ -17,13 +17,19 @@ function updateTotals(totalId, amount){
     totalchanged.innerText = newTotal;
     
 }
+// Get current balance
+function getCurrentBalance(){
+    const balance= document.getElementById('balance');
+    const balanceText = balance.innerText;
+    const previousBalance = parseFloat(balanceText);
+    return previousBalance;
+}
 
 // Update Balance
 
 function updateBalance(amount, isAdd){
     const balance= document.getElementById('balance');
-    const balanceText = balance.innerText;
-    const previousBalance = parseFloat(balanceText);
+    const previousBalance= getCurrentBalance();
     if(isAdd==true){
         balance.innerText = previousBalance + amount;
     }else{
@@ -35,14 +41,23 @@ function updateBalance(amount, isAdd){
 // Deposit
 document.getElementById('submitDeposit').addEventListener('click', function(){
     const depositAmount = getSubmitValue('deposit');
+    if(depositAmount > 0){
     updateTotals('total-deposited',depositAmount);
     updateBalance(depositAmount,true);
+    }
 });
 
 // Withdraw
 document.getElementById('submitWithdraw').addEventListener('click', function(){
     
     const withdrawAmount = getSubmitValue ('withdraw'); 
+    const currentBalance = getCurrentBalance();
+    
+    if(withdrawAmount > 0 && withdrawAmount < currentBalance){
     updateTotals('total-withdrawed', withdrawAmount)
     updateBalance(withdrawAmount,false);
+    }
+    if(withdrawAmount > currentBalance){
+        console.log("Withdraw Amount Exceeds Current Balance");
+    }
 })
