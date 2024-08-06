3dfe208834f452322b173cf33cde37c1373ed62d
shahriararefin <shahriararefin7@gmail.com>
2024-08-06 19:43:28 +0600

    added total Js

--- Projects/Module 27/app.js
+++ Projects/Module 27/app.js
@@ -15,30 +15,40 @@ function updateNumber(product, price, isIncreasing){
     input.value = inputCount;
     const phoneTotalPrice = document.getElementById(product + '-price');
     phoneTotalPrice.innerText = inputCount*price;
-    
-    // subtotal
-    const subtotalElement= document.getElementById('subtotal');
-    const subtotalText = subtotalElement.value;
-    const subtotal = parseInt(subtotalText)+ parseInt(phoneTotalPrice);
-    subtotalElement.innerText = subtotal;
+    calculateTotal();
+   
+}  
+
+function getInput(product){
+    const input= document.getElementById(product + '-count');
+    const totalInput = parseInt(input.value);
+    return totalInput;
 }
 
+function calculateTotal(){
+    const totalPhone = getInput('phone')*1219;
+    const totalCase = getInput('case')*59;
+    const subtotal = totalPhone + totalCase;
+    const tax = subtotal/10;  
+    const total = subtotal+tax;
+    document.getElementById('subtotal').innerText = subtotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total').innerText = total;
+
+   }
+
 document.getElementById('phone-plus').addEventListener('click', function(){
-    updateNumber('phone',1219,true);
-    
+    updateNumber('phone', 1219,true);
 });
 
 document.getElementById('phone-minus').addEventListener('click', function(){
-    updateNumber('phone',1219,false);
+    updateNumber('phone', 1219,false);
 });
 
-
 document.getElementById('case-plus').addEventListener('click', function(){
-    updateNumber('case',59,true);
+    updateNumber('case', 59,true);
 });
 
 document.getElementById('case-minus').addEventListener('click', function(){
-    updateNumber('case',59,false);
+    updateNumber('case', 59,false);
 });
-
-
