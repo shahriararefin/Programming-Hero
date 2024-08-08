d31cc3fe762773b55603f45be55bfeb418193ea0
shahriararefin <shahriararefin7@gmail.com>
2024-08-08 14:13:27 +0600

    slicing strings

---
+++ Projects/Module 28/Split-Slice/split.js
@@ -0,0 +1,7 @@
+const sliceStings= "it's not who i am underneath that defines me";
+
+const word= sliceStings.split(' ');
+console.log(word);
+const withoutA = sliceStings.split('a');
+console.log(withoutA);
+ 
