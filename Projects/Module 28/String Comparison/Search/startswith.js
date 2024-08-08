fff78bacf18dde3c37b887750e7992364d812284
shahriararefin <shahriararefin7@gmail.com>
2024-08-08 13:05:52 +0600

    basic js pt.2

---
+++ Projects/Module 28/String Comparison/Search/startswith.js
@@ -0,0 +1,28 @@
+const products = [
+    'Dell',
+    'iPhone 12',
+    'Lenovo Laptop',
+    'Lg Tv',
+    'Watch',
+    'iPhone 15',
+    'lg watch',
+    'dell pc',
+    'dell mini',
+    'iPhone 13',
+    'mac laptop',
+    'hp laptop',
+    'Smart Watch',
+    'Red Apple',
+];
+
+const search = 'dell';
+
+const output = [];
+
+for(const product of products){
+    if(product.toLowerCase().startsWith(search.toLowerCase())){
+        output.push(product);
+    }
+}
+
+console.log(output);
---
+++ Projects/Module 28/String Comparison/caseConvert.js
@@ -0,0 +1,9 @@
+const anthem = "Blah blah blah heu heu";
+const search = 'heo';
+
+const userInput = "blAckPink";
+const savedUserName = "blackPink";
+
+if (userInput.toLowerCase()== savedUserName.toLowerCase()) {
+    console.log('user exists');
+}
