ceb2fb73164c35f3d6b2b7994f73f438d1d50ba2
shahriararefin <shahriararefin7@gmail.com>
2024-08-27 09:20:46 +0600

    initialized JSX

--- Projects/explore-react-core-concepts/src/App.jsx
+++ Projects/explore-react-core-concepts/src/App.jsx
@@ -10,8 +10,29 @@ function App() {
     <>
      
      <h1>Hello React</h1>
+     <Person></Person>
+     <Person></Person>
+     <Student></Student>
       
     </>
+  )
+}
+
+function Person(){
+  const age = 43;
+  const person = {name: 'Honey', age:35}
+  return <h3>Hello there, {person.name}.Let me guess your age: {age}</h3>
+
+
+}
+
+function Student(){
+  return (
+  <div>
+    <h3>Student ID: </h3>
+    <p>Name: </p>
+    <p>Age</p>
+  </div>
   )
 }
 
