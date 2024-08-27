a14d26a74d6824774ac8dc6d8d455aed4a4a65b7
shahriararefin <shahriararefin7@gmail.com>
2024-08-27 14:46:20 +0600

    jsx props

--- Projects/explore-react-core-concepts/src/App.jsx
+++ Projects/explore-react-core-concepts/src/App.jsx
@@ -11,28 +11,61 @@ function App() {
      
      <h1>Hello React</h1>
      <Person></Person>
-     <Person></Person>
-     <Student></Student>
-      
+     <Student grade={10} score={89}></Student>
+     <Student grade="7" score="99"></Student>
+     <Developer></Developer>
+      <Device name="Laptop" price="135000"></Device>
+      <Device name="Mobile" price="190000"></Device>
     </>
   )
 }
 
 function Person(){
   const age = 43;
-  const person = {name: 'Honey', age:35}
-  return <h3>Hello there, {person.name}.Let me guess your age: {age}</h3>
+  const person = {name: 'Student', age:35}
+  return <h3>Hello there, {person.name}. Let me guess your age: {age}</h3>
 
 
 }
 
-function Student(){
+const {grade, score} = {grade: '7', score: '90'};
+
+function Student({grade, score}){
+  console.log(grade, score);
+  
   return (
-  <div>
-    <h3>Student ID: </h3>
-    <p>Name: </p>
-    <p>Age</p>
+  <div className='student'>
+    <h3>Student Info: </h3>
+    <p>Class: {grade} </p>
+    <p>Score: {score}</p>
   </div>
+  )
+}
+
+
+function Developer(){
+  const developerStyle ={
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '10px'
+  }
+  return (
+    <div style={developerStyle}>
+          <h5>Developer</h5>
+
+    </div>
+  )
+}
+
+
+function Device(props){
+  // console.log(props);
+  
+  return (
+    <div>
+      <h2>This Device:{props.name} Price: {props.price} </h2>
+    </div>
   )
 }
 
