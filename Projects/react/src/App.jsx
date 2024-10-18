7a1b9b3b34ddf1d1bce133301c5352b075e01ac7
shahriararefin <shahriararefin7@gmail.com>
2024-10-18 12:12:44 +0600

    jsx

--- Projects/react/src/App.jsx
+++ Projects/react/src/App.jsx
@@ -2,33 +2,35 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+// http://localhost:5173
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <About> </About>
+      <Student></Student>
+      
     </>
+  )
+}
+
+
+  
+
+function About(){
+  const person = {name: 'Shahriar', age: 25};
+
+  return <h2>Hello There, myself {person.name} of age {person.age} </h2>
+
+}
+
+function Student(){
+
+  return (
+  <div>
+    <h4>This is a student</h4>
+  </div>
   )
 }
 
